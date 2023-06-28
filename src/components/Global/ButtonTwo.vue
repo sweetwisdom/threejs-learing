9884bdf9197c0615ad466b7cd7702c39c8c4133d
sweetwisdom <57869305+sweetwisdom@users.noreply.github.com>
2023-06-28 21:45:53 +0800

    Initial commit

---
+++ src/components/Global/ButtonTwo.vue
@@ -0,0 +1,13 @@
+<template>
+    <el-button type="info">
+        <slot />
+    </el-button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'ButtonTwo',
+})
+</script>
